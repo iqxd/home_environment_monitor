5541ddf460f49a6fc0efb16bfb0160a8d73fabce
iqxd <327614680@qq.com>
2020-10-28 20:50:15 +0800

    add wireless and datetime modules

---
+++ datetime.py
@@ -0,0 +1,66 @@
+import machine
+import time
+import network
+import ntptime
+
+ntptime.NTP_DELTA = 3155644800
+ntptime.host = 'ntp1.aliyun.com'
+
+weekdays = {
+    1: 'Mon',
+    2: 'Tus',
+    3: 'Wen',
+    4: 'Thu',
+    5: 'Fri',
+    6: 'Sat',
+    7: 'Sun'
+}
+
+months = {
+    1: 'Jan',
+    2: 'Feb',
+    3: 'Mar',
+    4: 'Apr',
+    5: 'May',
+    6: 'Jun',
+    7: 'Jul',
+    8: 'Aug',
+    9: 'Sep',
+    10: 'Oct',
+    11: 'Nov',
+    12: 'Dec'
+}
+
+class DateTime:
+    def __init__(self,wl):
+        self._wl = wl
+        self._rtc = machine.RTC()
+    
+    def sync(self):
+        if not self._wl.isconnected():
+            if not self._wl.connect():
+                return -1
+        try:
+            ntptime.settime()
+            self._rtc = machine.RTC()
+        except:
+            return -2
+        else:
+            return 1
+    
+    def get(self):
+        year, month, day, weekday, hour, minute, second, _ = self._rtc.datetime()
+        weekday +=1
+        return year,month,day,weekday,hour,minute,second
+
+    def get_formatted(self, show_week = True, show_secs = False):
+        year,month,day,weekday,hour,minute,second = self.get()
+        if show_week:
+            fdate = "%s %02d-%s-%d" % (weekdays[weekday], day, months[month], year)
+        else:
+            fdate = "%02d-%s-%d" % (day, months[month], year)
+        if show_secs:
+            ftime = "%02d:%02d:%02d" % (hour, minute, second)
+        else:
+            ftime = "%02d:%02d" % (hour, minute)
+        return fdate, ftime
---
+++ wireless.py
@@ -0,0 +1,32 @@
+import network
+import time
+
+
+class Wireless:
+    def __init__(self,wifiname,password):
+        self._wlan =self._activate()
+        self._wifiname = wifiname
+        self._password = password
+        self.connect()
+
+    def _activate(self):
+        ap = network.WLAN(network.AP_IF)
+        if ap.active():
+            ap.active(False)
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
+        assert wlan.active()
+        return wlan
+
+    def connect(self):
+        self._wlan.connect(self._wifiname,self._password)
+        for i in range(15):
+            if self._wlan.isconnected():
+                return True
+            time.sleep(1)
+        else:
+            return False
+
+    def isconnected(self):
+        return self._wlan.isconnected()
+
