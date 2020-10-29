1e6361e223bd577fd69427733b460165b2b64ba2
iqxd <327614680@qq.com>
2020-10-29 20:44:01 +0800

    add log number in Logger

--- logger.py
+++ logger.py
@@ -1,18 +1,22 @@
 class Logger:
     def __init__(self, logfile):
         self._logfile = logfile
+        self._logno = 0
 
     def write_startlog(self, start_message):
         with open(self._logfile, 'a') as f:
             f.write(start_message)
 
-    def write_datalog(self, logno, logtimestr, temp, humi, co2, tvoc):
+    def write_datalog(self, logtimestr, temp, humi, co2, tvoc):
         with open(self._logfile, 'a') as f:
-            if logno == 1:
+            if self._logno == 0:
                 f.write('%4s %14s %2s %2s %4s %3s\n' %
                         ('No.', 'Datetime', 'TC', 'H%', 'CO2', 'TVOC'))
+                self._logno = 1
+
             f.write('%4d %14s %2d %2d %4d %3d\n' %
-                    (logno, logtimestr, temp, humi, co2, tvoc))
+                    (self._logno, logtimestr, temp, humi, co2, tvoc))
+            self._logno += 1
 
     def write_synclog(self, logtimestr, sync):
         if sync > 0:
